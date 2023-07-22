c6650c3054b4944abd39db1709c8af1a864de0d2
Breeze <big_fw@foxmail.com>
2023-07-22 14:20:05 +0800

    coding life

--- 10-1 软件I2C读写MPU6050/Hardware/MPU6050.c
+++ 10-1 软件I2C读写MPU6050/Hardware/MPU6050.c
@@ -73,4 +73,38 @@ void MPU6050_Init(void)
 	MPU6050_WriteReg(MPU6050_ACCEL_CONFIG, 0x18); // 加速度 最大量程
 }
 
-// void MPU6050_GetData()
+uint8_t MPU6050_GetID(void)
+{
+	return MPU6050_ReadReg(MPU6050_WHO_AM_I);
+}
+
+void MPU6050_GetData(int16_t *AccX, int16_t *AccY, int16_t *AccZ,
+												int16_t *GyroX, int16_t *GyroY, int16_t *GyroZ)
+{
+	uint8_t DataH, DataL;
+	// 加速度寄存器 XYZ轴
+	DataH = MPU6050_ReadReg(MPU6050_ACCEL_XOUT_H); // 高8位
+	DataL = MPU6050_ReadReg(MPU6050_ACCEL_XOUT_L); // 低8位
+	*AccX = (DataH << 8) | DataL;
+	
+	DataH = MPU6050_ReadReg(MPU6050_ACCEL_YOUT_H); 
+	DataL = MPU6050_ReadReg(MPU6050_ACCEL_YOUT_L); 
+	*AccY = (DataH << 8) | DataL;
+	
+	DataH = MPU6050_ReadReg(MPU6050_ACCEL_ZOUT_H); 
+	DataL = MPU6050_ReadReg(MPU6050_ACCEL_ZOUT_L); 
+	*AccZ = (DataH << 8) | DataL;
+	
+	// 陀螺仪
+	DataH = MPU6050_ReadReg(MPU6050_GYRO_XOUT_H); 
+	DataL = MPU6050_ReadReg(MPU6050_GYRO_XOUT_L); 
+	*GyroX = (DataH << 8) | DataL;
+	
+	DataH = MPU6050_ReadReg(MPU6050_GYRO_YOUT_H); 
+	DataL = MPU6050_ReadReg(MPU6050_GYRO_YOUT_L); 
+	*GyroY = (DataH << 8) | DataL;
+	
+	DataH = MPU6050_ReadReg(MPU6050_GYRO_ZOUT_H); 
+	DataL = MPU6050_ReadReg(MPU6050_GYRO_ZOUT_L); 
+	*GyroZ = (DataH << 8) | DataL;
+}
